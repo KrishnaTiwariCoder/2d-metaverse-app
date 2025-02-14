a59e01c85ef4d3810c82566786b38b6bb7c30cbd
KR$NA <78905769+KrishnaTiwariCoder@users.noreply.github.com>
2025-02-14 21:38:09 +0530

    added basic websockets

--- root/apps/ws/src/index.ts
+++ root/apps/ws/src/index.ts
@@ -1,7 +1,7 @@
 import { WebSocketServer } from "ws";
 import { connectDB } from "@repo/database";
 import { User } from "./User";
-import express from "express";
+import express, { Request, Response } from "express";
 import { createServer } from "https";
 import cors from "cors";
 
@@ -10,6 +10,10 @@ app.use(cors());
 const server = createServer(app);
 
 const wss = new WebSocketServer({ server });
+
+app.get("/", (req: Request, res: Response) => {
+  res.send("Hello World");
+}); // Health check
 
 connectDB();
 
@@ -24,4 +28,6 @@ wss.on("connection", function connection(ws) {
   });
 });
 
-server.listen(process.env.PORT || 3001);
+server.listen(process.env.PORT, () => {
+  console.log("Server is listening on port 3000");
+});
