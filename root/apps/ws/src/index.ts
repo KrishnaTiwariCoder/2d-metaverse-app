5ca44ef067eff6b1e1e57b7b13848be3d6647d23
KR$NA <78905769+KrishnaTiwariCoder@users.noreply.github.com>
2025-02-15 12:01:10 +0530

    bugs

--- root/apps/ws/src/index.ts
+++ root/apps/ws/src/index.ts
@@ -2,14 +2,27 @@ import { WebSocketServer } from "ws";
 import { connectDB } from "@repo/database";
 import { User } from "./User";
 import express, { Request, Response } from "express";
-import { createServer } from "https";
+import { createServer } from "http";
 import cors from "cors";
 
 const app = express();
 app.use(cors());
 const server = createServer(app);
 
-const wss = new WebSocketServer({ server });
+const wss = new WebSocketServer({
+  server,
+  clientTracking: true,
+  perMessageDeflate: {
+    zlibDeflateOptions: {
+      chunkSize: 1024,
+      memLevel: 7,
+      level: 3,
+    },
+    zlibInflateOptions: {
+      chunkSize: 10 * 1024,
+    },
+  },
+});
 
 app.get("/", (req: Request, res: Response) => {
   res.send("Hello World");
@@ -27,7 +40,7 @@ wss.on("connection", function connection(ws) {
     console.log("user disconnected");
   });
 });
-
-server.listen(process.env.PORT, () => {
-  console.log("Server is listening on port 3000");
+const port = process.env.PORT || 3001;
+server.listen(port, () => {
+  console.log("Server is listening on port" + port);
 });
