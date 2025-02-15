5ec858c1c0f88fc14308fe84aa2e1aa69ddfda13
KR$NA <78905769+KrishnaTiwariCoder@users.noreply.github.com>
2025-02-15 17:30:14 +0530

    fixed few errors

--- root/apps/client/src/utils/urls.ts
+++ root/apps/client/src/utils/urls.ts
@@ -1,2 +1,2 @@
-// export const WS_URL = "wss://twod-metaverse-app-tdb8.onrender.com";
-export const WS_URL = "ws://localhost:3001";
+export const WS_URL = "wss://twod-metaverse-app-tdb8.onrender.com";
+// export const WS_URL = "ws://localhost:3001";
